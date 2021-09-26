e889a1524ed4af9fa30054639a3bb30bca51cedc
WZY009 <2232213966@qq.com>
2021-09-26 11:17:53 +0800

    Pass

--- ProducerAndConsumer/MainWindow.xaml.cs
+++ ProducerAndConsumer/MainWindow.xaml.cs
@@ -21,7 +21,9 @@ namespace ProducerAndConsumer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int _bufferSize, _num, _producerNum, _consumberNum,_runTime;
+        //definite bufferSize, the number of product, the number of consumer & producer, the runnoing times.
+        private int _bufferSize, _productNum, _producerNum, _consumerNum, _runTime;
+        //definite Semaphore and Mutex.
         private Semaphore _full, _empty;
         private Mutex _mutex;
 
@@ -32,21 +34,19 @@ namespace ProducerAndConsumer
         }
         public void createthread()
         {
-            Thread[] consumerThread = new Thread[_consumberNum];//create the number of consumer threading.
+            Thread[] consumerThread = new Thread[_consumerNum];//create the number of consumer threading.
             Thread[] producerThread = new Thread[_producerNum];
             for(int i = 0; i < _producerNum; i++)
             {
-                producerThread[i] = new Thread(new ParameterizedThreadStart(produce));
-                producerThread[i].IsBackground = true;
-                producerThread[i].Start(i + 1);
-                Thread.Sleep(100);
+                producerThread[i] = new Thread(new ParameterizedThreadStart(produce));//create the threading
+                producerThread[i].IsBackground = true;//when the windows threading is killed, the background threading will be killed too.
+                producerThread[i].Start(i + 1);//start running    
             }
-            for(int j = 0; j < _consumberNum; j++)
+            for(int j = 0; j < _consumerNum; j++)
             {
                 consumerThread[j] = new Thread(new ParameterizedThreadStart(consume));
                 consumerThread[j].IsBackground = true;
                 consumerThread[j].Start(j + 1);
-                Thread.Sleep(100);
             }
         }
 
@@ -55,7 +55,7 @@ namespace ProducerAndConsumer
             Showlist.Text = " ";
             _bufferSize = int.Parse(bufferSize.Text);
             _producerNum = int.Parse(producer.Text);
-            _consumberNum = int.Parse(consumer.Text);
+            _consumerNum = int.Parse(consumer.Text);
             _full = new Semaphore(0, _bufferSize);
             _empty = new Semaphore(_bufferSize, _bufferSize);
             _mutex = new Mutex();
@@ -67,9 +67,10 @@ namespace ProducerAndConsumer
             {
                 _empty.WaitOne();
                 _mutex.WaitOne();
-                _num++;
                 _runTime++;
-                this.Dispatcher.Invoke(() => { Showlist.AppendText(_runTime + "th producer  " + "The position is:" + id.ToString() + "  " + (_bufferSize - _num)+"\n"); });
+                _productNum++;
+                this.Dispatcher.Invoke(() => { Showlist.AppendText(_runTime + "th running times     " + "The producer is:" + id.ToString() + "     You have " + (_bufferSize - _productNum) + " free position" + "\n"); });
+                Thread.Sleep(1000);//sleep a while or you will feel too fast to look at the process               
                 _mutex.ReleaseMutex();
                 _full.Release();
             }
@@ -80,9 +81,10 @@ namespace ProducerAndConsumer
             {
                 _full.WaitOne();
                 _mutex.WaitOne();
-                _num--;
+                _productNum--;
                 _runTime++;
-                this.Dispatcher.Invoke(() => { Showlist.AppendText(_runTime + "th consumer  " + "The free position is:" + id.ToString() + "  " + (_bufferSize - _num) + "\n"); });
+                this.Dispatcher.Invoke(() => { Showlist.AppendText(_runTime + "th running times     " + "The consumer is:" + id.ToString() + "     You have " + (_bufferSize - _productNum) + " free position" + "\n"); });
+                Thread.Sleep(1000);
                 _mutex.ReleaseMutex();
                 _empty.Release();
             }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WindowsOSHomework
-My homework of Windows OS
+## Homework1 -- producer and consumer problem
+I use WPF to describe producer and consumber problem. All the explaination has been written.
